dbce15f608d3fc44e828b28beb14fbcf64874986
szadkrud_homepc <szadkrud@fel.cvut.cz>
2021-02-04 19:26:32 +0100

    extended doc

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ This package contains implementation of **ens**emble of **gen**erative models wi
  and the framework for continual learning evaluation.
  
 ## Requirements
-Tested on python 3.7.4. (only CPU) and 2.7. (with GPU).
+For running ensgendel with GPU, [cupy](https://cupy.dev/) is required.
+Tested on python 3.7.4. (only CPU) and 2.7. (with GPU). Excerpts from `pip freeze` command are below.
 
 Python 3.7.4. setup:\
 chainer==7.7.0
@@ -24,10 +25,18 @@ scikit-learn==0.19.1
 scipy==1.0.0
 
 ## Run demo
-From the project root run the demo application *incremental_evaluation_run.py*\
-some command \
-The application creates results/incremental_evaluation_run/ file where the app stores all results.
+1. From the project root run the demo application *incremental_evaluation_run.py*\
+`python incremental_evaluation_run.py exp1 mnist_cn5 12345 --debug True`\
+The application creates *results/incremental_evaluation_run/* file where app stores results of *exp1* experiment.
+The `--debug True` option runs only ligthweight classifiers, so this run just tests whether everything works.
+2. If no errors showed up in from previous task run\
+`python incremental_evaluation_run.py exp2 mnist012 234 --scout 1000 -trials 3`\
+which creates experiment *exp2* where all predictors are evaluated on *mnist012* scenarios, which operate on pruned
+dataset. The results are averaged from three trials.
+3. For more options consult the help option\
+`python incremental_evaluation_run.py`
 
+If you have CUDA and *cupy* installed, turn on the GPU by 
 ## Adding new continual learners and scenarios
 You can integrate new continual learners and scenarios by implementing the interfaces **Predictor** and **ScenarioSet**,
  respectively. See interface definitions in *incremental_evaluation/interfaces* for details.
--- incremental_evaluation/interfaces.py
+++ incremental_evaluation/interfaces.py
@@ -1,35 +1,75 @@
 import numpy as np
 DEFAULT_CLASSES = (0, 1)
-
+"""
+These interfaces should be used if you want to expand this framework with new predictors and scenario sets.
+"""
 
 class Predictor(object):
     def __init__(self, classes=DEFAULT_CLASSES):
+        """
+        The interface for classifier learning algorithms. The only constructor input is optional classes. Nothing
+        else is expected. The parametrization should be done within this class.
+        @param classes: The only input
+        """
         self._predict = lambda X: np.empty((0, ))
         self._fit = lambda X, y: None
         self._classes = list(classes)
 
     def predict(self, X):
+        """
+        Transforms features X into labels y.
+        @param X: np.ndarray of shape (number_of_instances, feature_dimension)
+        @return: feature labels np.ndarray of shape (number_of_instances, ) and type np.int
+        """
         return self._predict(X)
 
     def fit(self, X, y):
+        """
+        Trains on given training set. This is a modifier type function, it should modify the model stored in this class.
+        @param X: features, np.ndarray of shape (number_of_instances, feature_dimension)
+        @param y: feature labels np.ndarray of shape (number_of_instances, ) and type np.int
+        @return: optional, can be None.
+        """
         return self._fit(X, y)
 
 
 class ScenarioSet(object):
     def __init__(self):
-        self._scenarios = []
-        self._testing_subclasses = np.empty((0, ))
-        self._testing_samples = np.empty((0, 0))
-        self._training_subclasses = np.empty((0, ))
-        self._training_samples = np.empty((0, 0))
+        """
+        Class producing set of scenarios constructed from subclasses.
+        Each scenario is defined as a list of tasks, and each task is defined as a label-subclasses assignments:
+
+        [{hard_terrain: [swamp], easy_terrain: [road, snow]}, {hard_terrain: [snow]}]
+        Here we have two tasks. In the first one the classifier gets images of swamp labeled as hard_terrain and
+        the road and snow images are labeled as easy_terrain. In the second task the classifier trains on a new dataset
+        where the snow images have hard_terrain label (label of snow changed). The testing task would be instances of
+        swamp and snow labeled as hard_terrain and road images labeled as easy_terrain.
+
+        In this example the swamp, road, and snow are the subclasses, where the images are samples and
+        terrain types identifiers are subclasses. These subclasses are explicitly known only to scenario
+        construction methods. The classifier then gets only the images and labels.
+        In the case of the example there are two labels: hard_terrain, easy_terrain.
+
+        In practice, we index the subclasses and labels with integers. So the example scenario would be encoded as
+        [{0: [0], 1: [1, 2]}, {0: [2]}]
+        """
+        # Overwrite this constructor and the private attributes.
+        self._scenarios = []  # list of scenarios [[{label: [subclass, ...], ...}, ...], ...]
+        self._testing_subclasses = np.empty((0, ))  # testing subclasses shape: (num_of_test_samples, )
+        self._testing_samples = np.empty((0, 0))  # testing samples, shape: (num_of_test_samples, feature_dimension)
+        self._training_subclasses = np.empty((0, ))  # testing subclasses shape: (num_of_train_samples, )
+        self._training_samples = np.empty((0, 0))  # testing samples, shape: (num_of_train_samples, feature_dimension)
 
     def get_training_set(self):
+        #  Do not overwrite this
         return self._training_samples, self._training_subclasses
 
     def get_test_set(self):
+        #  Do not overwrite this
         return self._testing_samples, self._testing_subclasses
 
     def get_scenarios(self):
+        #  Do not overwrite this
         return self._scenarios
 
 
